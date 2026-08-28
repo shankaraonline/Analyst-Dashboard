a1586fe67fa54c2f0a60392bbcc591f58b3d282e
Web-Pages <jadhavdarshan0005@gmail.com>
2026-08-28 17:11:59 +0530

    fix: use root base path for subdomain deployment at viewpoint.shankaraonlinesolutions.com

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Required for subdirectory deployment at shankaraonlinesolutions.com/viewpoint
-  base: '/viewpoint/',
+  // Deployed at viewpoint.shankaraonlinesolutions.com (root of subdomain)
+  base: '/',
 })
+
 
